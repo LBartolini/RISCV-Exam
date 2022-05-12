ff9d869042a1b08c3f500959889a910470ff6110
LBartolini <l.bartolini02@gmail.com>
2022-05-12 14:04:00 +0200

    commentato inversione

--- inversione.s
+++ inversione.s
@@ -5,6 +5,10 @@ inversione_stringa:
 #! a0 a1 a2 a3, a4 t0
 #! manage_ra
 
+# scorro la stringa con i fino ad n/2 (con n lunghezza della stringa)
+# ed eseguo una swap dei caratteri in posizione i e n-i
+
+# calcolo la lunghezza della stringa in a1
 #! precall(str_len)
 jal str_len
 addi a1, a0, 0
@@ -13,8 +17,14 @@ addi a1, a0, 0
 srli a2, a1, 1 # divido per due per sapere quando fermarmi
 li t0, 0 # indice per scorrere la stringa
 
+# nb: dividendo per due con srli eseguo un troncamento in caso di lunghezza dispari
+# questo però non mi interessa perchè in caso di lunghezza dispari il carattere centrale
+# rimarrà nella stessa posizione
+
 loop_inversione_stringa:
 beq t0, a2, fine_inversione_stringa
+
+# prendo i due caratteri da invertire
 add t1, a0, t0
 lb a3, 0(t1) # a3 = stringa[t0]
 add t1, a0, a1
@@ -32,7 +42,6 @@ sb a3, 0(t1)
 
 addi t0, t0, 1
 j loop_inversione_stringa
-
 
 fine_inversione_stringa:
 #! end
